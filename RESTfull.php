fb954e81c5e8958b39192ce983ca003a3db64872
fanrong33 <fanrong33@qq.com>
2015-06-06 23:57:32 +0800

    add RESTfull.php

---
+++ RESTfull.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * Created on 2013-4-23
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+
+@request method GET
+
+$this->ajaxReturn(null, '接口key密钥不存在', 0);
+$this->ajaxReturn(null, '请求的HTTP METHOD不支持，请检查是否选择了正确的POST/GET方式', 0);
+
+order_by
+direction
+
+$params = array();
+$params['m'] 		= 'thumb';
+$params['image_url']= str_replace('|', '/', $rs['imgUrl']);
+$params['width'] 	= 190;
+$params['height']	= 140;
+
+$tmp_scenic['imgUrl']			= pack_url('api2.php?'.http_build_query($params)); 	// 景点图片
+
+?>
